6721a2cb09899e40de0208f38b4b7177e6b3de2c
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-01-06 17:21:29 +0530

    Delete var.tf

--- Terraform-master/var.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  type        = string
-  description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
-}
-
-variable "location" {
-  type        = string
-  description = "(Required) The location where the resource group should be created. For a list of all Azure locations, please consult this link or run az account list-locations --output table."
-}
