7523ed02cba0f4fb6a0b4a0f4d3b8332f6b7949e
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-01-06 18:42:17 +0530

    Create var.tf

---
+++ Terraform-master/var.tf
@@ -0,0 +1,12 @@
+
+variable "name" {
+  type        = string
+  description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
+  default= "sampleRG"
+}
+
+variable "location" {
+  type        = string
+  description = "(Required) The location where the resource group should be created. For a list of all Azure locations, please consult this link or run az account list-locations --output table."
+  default= "eastus"
+}
