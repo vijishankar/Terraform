793874f845d3ec7a0cedabe3fd3e14d7ba9773c0
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-01-07 11:26:00 +0530

    Update var.tf

--- Terraform-master/var.tf
+++ Terraform-master/var.tf
@@ -1,15 +1,15 @@
 
- variable "name" {
-  type        = string
- description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
-  default= "sampleRG"
-}
+# variable "name" {
+#  type        = string
+# description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
+#  default= "sampleRG"
+#}
 
-variable "location" {
-  type        = string
-  description = "(Required) The location where the resource group should be created. For a list of all Azure locations, please consult this link or run az account list-locations --output table."
-  default= "eastus"
-}
+#variable "location" {
+#  type        = string
+#  description = "(Required) The location where the resource group should be created. For a list of all Azure locations, please consult this link or run az account list-locations --output table."
+#  default= "eastus"
+#}
 
 #variable "resource_group_name" {
  # description = "Default resource group name that the network will be created in."
