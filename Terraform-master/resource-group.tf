d9c64cfdae944b483972845dd551cd2866e16780
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-01-07 11:30:23 +0530

    Update resource-group.tf

--- Terraform-master/resource-group.tf
+++ Terraform-master/resource-group.tf
@@ -23,17 +23,17 @@ resource "azurerm_resource_group" "example" {
   location = "eastus"
 }
 
-#resource "azurerm_application_insights" "example" {
-#  name                = "tf-test-appinsights"
-#  location            = azurerm_resource_group.example.location
-#  resource_group_name = azurerm_resource_group.example.name
-#  application_type    = "web"
-#}
+resource "azurerm_application_insights" "example" {
+  name                = "tf-test-appinsights"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  application_type    = "web"
+}
 
-#output "instrumentation_key" {
-#  value = azurerm_application_insights.example.instrumentation_key
-#}
+output "instrumentation_key" {
+  value = azurerm_application_insights.example.instrumentation_key
+}
 
-#output "app_id" {
-#  value = azurerm_application_insights.example.app_id
-#}
+output "app_id" {
+  value = azurerm_application_insights.example.app_id
+}
