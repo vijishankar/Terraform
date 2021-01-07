277bdb0a471ef14d5866833c91f2a4f30704bff2
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-01-07 11:04:25 +0530

    Update resource-group.tf

--- Terraform-master/resource-group.tf
+++ Terraform-master/resource-group.tf
@@ -7,13 +7,13 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "cloudcommons" {
-#  name     = var.name
-#  location = var.location
-#}
-
-resource "azurerm_resource_group" "test" {
-  count    = 2
-  name     = element(var.resource_group_name, count.index)
-  location = var.location
+resource "azurerm_resource_group" "cloudcommons" {
+ name     = var.name
+ location = var.location
 }
+
+#resource "azurerm_resource_group" "test" {
+#  count    = 2
+ # name     = element(var.resource_group_name, count.index)
+ # location = var.location
+#}
